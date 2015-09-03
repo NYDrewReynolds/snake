b8b74ab1f6a1a29566a718849ba6811402487107
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-09-03 15:00:36 -0600

    Test Update

--- test/eat-food.js
+++ test/eat-food.js
@@ -1,19 +1,19 @@
-describe('Snake Eats', function () {
-  xit('food', function () {
+describe('Eating food', function () {
+  xit('makes snake longer', function () {
     expect(snake.bodyArray.length).to.equal(5)
     // new Food
     // snake.determineCell() to = food x,y
     expect(snake.bodyArray.length).to.equal(6)
   });
 
-  xit('food & updates score', function () {
+  xit('updates score', function () {
     expect(score).to.equal(0)
     // new Food
     // snake.determineCell() to = food x,y
     expect(score).to.equal(10)
   });
 
-  xit('food & updates level', function () {
+  xit('updates level', function () {
     expect(level).to.equal(1)
     // new Food
     // snake.determineCell() to = food x,y
